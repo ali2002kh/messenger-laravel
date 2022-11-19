d7311efbd3f10d0a83513f20ee68d663ae31fb3d
ali2002kh <73595075+ali2002kh@users.noreply.github.com>
2022-11-19 15:32:16 +0330

    front for auth

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,20 +1,33 @@
-@extends('layouts.app')
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login</title>
+    <link rel="stylesheet" href="css/login.css">
+</head>
+<body>
+    <form action="{{ route('login') }}" method='Post'>
+        <h1>Login Here</h1>
+        @if ($errors->any())
+            <ul>
+                @foreach ($errors->all() as $error)
+                    <li>{{ $error }}</li>
+                @endforeach
+            </ul>
+        @endif
+        @csrf
+        <label for="Phone">Phone Number</label>
+        <input id='tel' type='text' name='number' placeholder='number' value="{{ old('number') }}" required>
 
-@section('title')
-login
-@endsection
+        <label for="password">Password</label>
+        <input type="password" name='password' placeholder="password" id="password" required>
 
-@section('content')
+        <button type="submit">Log In</button>
 
-@include('layouts.partials.error')
+        <a href="{{ route('signup_page') }}">Sing Up</a>
+    </form>
+</body>
+</html>
 
-<form action="{{ route('login') }}" method='Post'>
-    @csrf
-    <input type='text' name='number' placeholder='number' value="{{ old('number') }}">
-    <input type='password' name='password' placeholder='password'>
-    <input type='submit'>
-</form>
-
-<a href="{{ route('signup_page') }}">sign up</a>
-
-@endsection
--- resources/views/auth/signup.blade.php
+++ resources/views/auth/signup.blade.php
@@ -1,22 +1,38 @@
-@extends('layouts.app')
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Singup</title>
+    <link rel="stylesheet" href="css/singup.css">
+</head>
+<body>
+    <form action="{{ route('signup') }}" method='Post'>
+        <h1>Signup Here</h1>
+        @if ($errors->any())
+            <ul>
+                @foreach ($errors->all() as $error)
+                    <li>{{ $error }}</li>
+                @endforeach
+            </ul>
+        @endif
+        @csrf
+        <label for="Phone">Phone Number</label>
+        <input type="text" name='number' placeholder="number" id="tel" required>
 
-@section('title')
-signup
-@endsection
+        <label for="Username">Username</label>
+        <input type="text" name='username' placeholder="username" id="text" required>
 
-@section('content')
+        <label for="password">Password</label>
+        <input type="password" name='password1' placeholder="password" id="password" required>
 
-@include('layouts.partials.error')
+        <label for="cpassword">Confirm Password</label>
+        <input type="password" name="password2" placeholder="confirm password" id="cpassword" required>
 
-<form action="{{ route('signup') }}" method='Post'>
-    @csrf
-    <input type='text' name='number' placeholder='number'>
-    <input type='text' name='username' placeholder='username'>
-    <input type='password' name='password1' placeholder='password'>
-    <input type='password' name='password2' placeholder='confirm password'>
-    <input type='submit'>
-</form>
+        <button type="submit">Sign Up</button>
 
-<a href="{{ route('login_page') }}">login</a>
-
-@endsection
+        <a href="{{ route('login_page') }}">Log in</a>
+    </form>
+</body>
+</html>
