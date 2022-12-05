b6938bed99891027feff0d681698d434f7dc6c6f
Nima Shahrokhi <nima2002sh@gmail.com>
2022-12-05 21:38:36 +0330

    edit signup and login pages

--- resources/views/profile/create.blade.php
+++ resources/views/profile/create.blade.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>create profile</title>
+    <title>ساخت پروفایل</title>
     <link rel="stylesheet" href="../css/edit.css">
 </head>
 <body>
@@ -12,11 +12,15 @@
             enctype="multipart/form-data">
         @csrf
         <img src="{{ asset('storage/img/default.jpg') }}">
+        <label for="file">عکس پروفایل</label>
         <input type="file" class="myfile" name="file" required><br>
-        <input type="text" class="fname" name="fname" placeholder="first name" required><br>
-        <input type="text" class="lname" name="lname" placeholder="last name" required><br>
-        <textarea name='bio' placeholder='bio' required></textarea>
-        <input type='submit'>
+        <label for="fname">نام</label>
+        <input type="text" class="fname" name="fname" placeholder="نام" required><br>
+        <label for="lname">نام خانوادگی</label>
+        <input type="text" class="lname" name="lname" placeholder="نام خانوادگی" required><br>
+        <label for="bio">بایو</label>
+        <textarea name='bio' placeholder='بایو' required></textarea><br>
+        <input type='submit' value="ثبت" class="submit">
     </form>
 </body>
 </html>
